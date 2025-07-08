22e66b4f351e766bc96398657b75775c6143d253
kim <smkim000721@naver.com>
2025-07-08 17:41:55 +0900

    로그인페이지 수정

--- src/component/login.js
+++ src/component/login.js
@@ -2,25 +2,23 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-    const [id,setId] = useState('');
-    const [pw,setPw] = useState('');
+    const [user,setUser] = useState({id:'',pw:''});
     const nav = useNavigate();
 
     function login(){
         fetch('http://localhost/login',{
             method:'POST',headers:{"Content-Type":"application/json"},
-            body: JSON.stringify({id:id,pw:pw}),credentials: 'include',
+            body: JSON.stringify({id:user.id,pw:user.pw}),credentials: 'include',
         }).then(async (res)=>{
             // res.text() -> 서버에서 응답하는 문자열 
             const text = await res.text();
             if(res.ok && text === '로그인 성공'){
                 alert('로그인 성공');
                 console.log('성공');
-                nav('/MyPage');
+                nav('/MainPage');
             }else{
                 alert('로그인 실패');
-                setId('');
-                setPw('');
+                
             }
         })
     }
@@ -31,13 +29,14 @@ export default function Login() {
                 <tbody>
                 <tr>
                     <th>아이디</th>
-                    <td><input type="text" value={id} onChange={(e)=>{setId(e.target.value)}}/></td>
+                                                                                 {/*기존값 유지하고 변경값만 덮어쓴다 */}
+                    <td><input type="text" value={user.id} onChange={(e)=>{setUser({...user,id:e.target.value})}}/></td>
                 </tr>
                 </tbody>
                 <tbody>
                 <tr>
                     <th>비밀번호</th>
-                    <td><input type="password" value={pw} onChange={(e)=>{setPw(e.target.value)}}/></td>
+                    <td><input type="password" value={user.pw} onChange={(e)=>{setUser({...user,pw:e.target.value})}}/></td>
                 </tr>
                 </tbody>
             </table>
