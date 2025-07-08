f4d485cd84a083c929482f273dcd46b5ad32e127
kim <smkim000721@naver.com>
2025-07-08 17:40:24 +0900

    페이지 이동 루트 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './component/Login';
+import MyPage from './component/MyPage'
+import MainPage from './component/MainPage';
+import SearchPage from './component/SearchPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <div>
+
+            <Routes>
+                <Route path='/' element={<Login/>}></Route>
+                <Route path='/MainPage' element={<MainPage/>}></Route>
+                <Route path='/MyPage/:userId' element={<MyPage/>}></Route>
+                <Route path='/SearchPage' element={<SearchPage/>}></Route>
+            </Routes>
+        </div>
+
+        {/*Footer */}
+        <div>
+            
+        </div>
+    </BrowserRouter>
   );
 }
 
