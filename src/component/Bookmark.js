9aa0c0586716407cdb59c6969a1ce23dad10cbc3
kim <smkim000721@naver.com>
2025-07-23 17:41:03 +0900

    즐겨찾기 목록

--- src/component/Bookmark.js
+++ src/component/Bookmark.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 export default function Bookmark() {
     const{userId} = useParams();
+    const[store,setStore] = useState({});
+
+    useEffect(()=>{
+        fetch('http://localhost/bookmarkList?userId='+userId)
+        .then((res)=>(res.json()))
+        .then((data)=>{setStore(data)})
+    },[userId])
     return (
         <div>
             <h1>즐겨찾기</h1>
