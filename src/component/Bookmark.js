e719a2bdb8cd5b0e08fe55cbab02c52ca9870906
kim <smkim000721@naver.com>
2025-07-25 13:23:16 +0900

    즐겨찾기 리스트 추가

--- src/component/Bookmark.js
+++ src/component/Bookmark.js
@@ -3,16 +3,36 @@ import { useParams } from 'react-router-dom'
 
 export default function Bookmark() {
     const{userId} = useParams();
-    const[store,setStore] = useState({});
+    const[storeList,setStoreList] = useState([]);
 
     useEffect(()=>{
         fetch('http://localhost/bookmarkList?userId='+userId)
         .then((res)=>(res.json()))
-        .then((data)=>{setStore(data)})
+        .then((data)=>{setStoreList(data)})
     },[userId])
     return (
         <div>
             <h1>즐겨찾기</h1>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>가게</th>
+                        <th>종류</th>
+                        <th>위치</th>
+                    </tr>
+                </tbody>
+                <tbody>
+                    {
+                        storeList.map((list)=>(
+                            <tr>
+                                <td>{list.title}</td>
+                                <td>{list.category}</td>
+                                <td>{list.address}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
