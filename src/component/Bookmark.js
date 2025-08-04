a9f11e511d6d9d9bd13821283e93e622506fd36b
kim <smkim000721@naver.com>
2025-08-04 16:37:48 +0900

    즐겨찾기 페이지에서 즐겨찾기 삭제

--- src/component/Bookmark.js
+++ src/component/Bookmark.js
@@ -10,6 +10,25 @@ export default function Bookmark() {
         .then((res)=>(res.json()))
         .then((data)=>{setStoreList(data)})
     },[userId])
+
+    function deleteBookmark(userId,storeId){
+        if(window.confirm('삭제하시겠습니까?')){
+            fetch('http://localhost/deleteBookmark', {method:'DELETE',
+              headers:{"Content-Type":"application/json"},
+              body: JSON.stringify({userId:userId,storeId:storeId})})
+            .then((res)=>{
+                if(res.ok){
+                  // 삭제 후 새로고침
+                  window.location.reload();
+                }else{
+                    alert('삭제실패');
+                }
+            })
+        }else{
+            alert('삭제취소');
+        }
+    }
+
     return (
         <div>
             <h1>즐겨찾기</h1>
@@ -19,15 +38,17 @@ export default function Bookmark() {
                         <th>가게</th>
                         <th>종류</th>
                         <th>위치</th>
+                        <th></th>
                     </tr>
                 </tbody>
                 <tbody>
                     {
                         storeList.map((list)=>(
                             <tr>
-                                <td><a href='{list.link}'>{list.title}</a></td>
+                                <td><a href={list.link}>{list.title}</a></td>
                                 <td>{list.category}</td>
                                 <td>{list.address}</td>
+                                <td><button onClick={()=>deleteBookmark(userId,list.storeId)}>삭제</button></td>
                             </tr>
                         ))
                     }
