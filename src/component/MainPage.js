0d46ea55b62f5e2674e470788f1379159abdb1ca
kim <smkim000721@naver.com>
2025-08-04 16:39:15 +0900

    메인페이지에 즐겨찾기별, 리뷰별 순위

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -1,11 +1,62 @@
-import React from 'react'
-import Header from './Header'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function MainPage() {
+    const[bookmarkRank,setBookmarkRank] = useState([]);
+    const[reviewRank,setReviewRank]=useState([]);
+
+    useEffect(()=>{
+            fetch('http://localhost/selectStoreRankByBookmark')
+            .then((res)=>(res.json()))
+            .then((data)=>{setBookmarkRank(data)})
+    },[])
+
+    useEffect(()=>{
+            fetch('http://localhost/selectStoreRankByReview')
+            .then((res)=>(res.json()))
+            .then((data)=>{setReviewRank(data)})
+    },[])
+
     return (
         <div>
             <h1>메인페이지</h1>
+                <h3>즐겨찾기 순위</h3>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>순위</th>
+                            <th>이름</th>
+                        </tr>
+                    </tbody>
+                    {
+                        bookmarkRank.map((list)=>(
+                            <tbody>
+                                <tr>
+                                    <td>{list.ranking}</td>
+                                    <td>{list.title}</td>
+                                </tr>
+                            </tbody>
+                    ))}
+                </table>
+
+                <h3>리뷰 순위</h3>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>순위</th>
+                            <th>이름</th>
+                        </tr>
+                    </tbody>
+                    {
+                        reviewRank.map((list)=>(
+                            <tbody>
+                                <tr>
+                                    <td>{list.ranking}</td>
+                                    <td>{list.title}</td>
+                                </tr>
+                            </tbody>
+                    ))}
+                </table>
             <Link to={'/SearchPage'}>위치찾기</Link>
         </div>
     )
