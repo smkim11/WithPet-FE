dcdb7bf8f58d4d8e9382af16340aaea765d083f1
kim <smkim000721@naver.com>
2025-08-08 12:43:45 +0900

    헤더에 메인페이지로 이동가능한 로고추가

--- src/component/Header.js
+++ src/component/Header.js
@@ -29,11 +29,45 @@ export default function Header() {
 
     return (
         
-        <div>
+    <header className="flex justify-between items-center px-6 py-3 bg-white shadow-md sticky top-0 z-50">
+      <div>
+        <Link to="/MainPage">
+          <img
+            src="/image/logo1.png"
+            alt="With Pet 로고"
+            className="h-12 cursor-pointer w-30 h-40"
+          />
+        </Link>
+      </div>
+
+      <div className="flex items-center space-x-8">
+        <div className="text-gray-800">
+          <span className="block text-lg font-semibold mb-1 border-b-2 border-orange-400 inline-block pb-1">
             {user?.name}님 환영합니다!
-            <Link to={'/MyPage/'+user.userId}>마이페이지</Link> &nbsp;
-            <Link to={'/Bookmark/'+user.userId}>즐겨찾기</Link><br/>
-            <button onClick={logout}>로그아웃</button>
+          </span>
+          <div className="flex space-x-6 text-sm text-gray-600">
+            <Link
+              to={`/MyPage/${user.userId}`}
+              className="hover:text-orange-500 transition"
+            >
+              마이페이지
+            </Link>
+            <Link
+              to={`/Bookmark/${user.userId}`}
+              className="hover:text-orange-500 transition"
+            >
+              즐겨찾기
+            </Link>
+          </div>
         </div>
+
+        <button
+          onClick={logout}
+          className="bg-orange-500 hover:bg-orange-600 text-white font-semibold px-4 py-2 rounded-md shadow transition"
+        >
+          로그아웃
+        </button>
+      </div>
+    </header>
     )
 }
